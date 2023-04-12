39d6cd1625901201cc62700606b72f3361e5762c
filip <filip.pandi@inovatrend.com>
2023-04-12 14:45:50 +0200

    Commented out cors headers config for local usage.

--- next.config.js
+++ next.config.js
@@ -1,31 +1,26 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  reactStrictMode: false
-}
-
 module.exports = {
-  async headers() {
-    return [
-      {
-        // Set CORS headers for all responses
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       // Set CORS headers for all responses
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           key: "Access-Control-Allow-Origin",
+  //           value: "*",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
   reactStrictMode: false
 };
